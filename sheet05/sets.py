4825c709caba6c22f1be149d09dc1e1cce0334b7
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2014-11-23 18:22:40 +0100

    add sets.py

---
+++ sheet05/sets.py
@@ -0,0 +1,24 @@
+def subsets(s, k):
+    result = set()
+    for e in s:
+        if len(e) == k:
+            result.add(e)
+    return result
+
+def permutations(s, k):
+    return set(perm_helper(s, k))
+
+def perm_helper(s, k):
+    out = []
+    if len(s) == 1:
+        out = set(s)
+    else:
+        for e in s:
+            if k > 1:
+                t = s.copy()
+                t.remove(e)
+                for perm in perm_helper(t, k-1):
+                    out.append(tuple(e) + tuple(perm))
+            else:
+                out.append(e)
+    return out
