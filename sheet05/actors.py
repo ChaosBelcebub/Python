74f9650010b69a83d361bbd2c7abfaaafe60fe24
ChaosBelcebub <mich3ael@web.de>
2014-11-24 17:05:21 +0100

    add actors.py

---
+++ sheet05/actors.py
@@ -0,0 +1,36 @@
+"""Actors with dictionary, WS1415
+
+References:
+ * Lecture slides at:
+   http://www.informatik.uni-freiburg.de/~ki/teaching/ws1415/info1/lecture.html
+ * Python documentation at:
+   http://docs.python.org/3/
+
+"""
+
+actors = [[("salutation", "Mr."), ("givenName", "Peter"),
+           ("familyName", "Dinklage"), ("birthday", "06/11/1969"),
+           ("GoT", "Lannister", "Tyrion")],
+          [("salutation", "Mrs."), ("givenName", "Michelle"), 
+           ("familyName", "Fairley"), ("birthday", "01/17/1964"),
+           ("GoT", "Stark", "Catelyn")],
+          [("salutation", "Mr."), ("givenName", "Nicolaj"),
+           ("familyName", "Coster-Waldau"), ("birthday", "07/27/1970"),
+           ("GoT", "Lannister", "Jamie")],
+          [("salutation", "Ms."), ("givenName", "Maisie"),
+           ("familyName", "Williams"), ("birthday", "04/17/1997"),
+           ("GoT", "Stark", "Arya")],
+          [("salutation", "Mrs."), ("givenName", "Emilia"),
+           ("familyName", "Clarke"), ("birthday", "05/01/1987"),
+           ("GoT", "Targaryen", "Daenerys")]]
+
+
+
+## Your functions follow here
+## ...
+
+
+if __name__ == "__main__":
+    # Your tests follow here after you deleted the next line:
+    pass
+    # Your print command for exercise 5.3b follows here:
