d69753f4d04e9441c88bc8ff8a0c7bb9a443d9c0
ChaosBelcebub <mich3ael@web.de>
2015-01-15 17:19:22 +0100

    finished code

---
+++ sheet10/augc.py
@@ -0,0 +1,19 @@
+import re
+
+# RegEx sind Sinnvoll da der Code so um einiges kürzer ist
+def check_for_startcodon(AUGCstring):
+    """Checks for startcodon
+
+    Args:
+      AUGCstring (string): the AUGC string
+    """
+    if re.findall(r"AUG", AUGCstring):
+        return True
+    else:
+        return False
+
+#b) r"AUG(?:[AUGC]{3})+UAG"
+
+def extract_codon_string(AUGCstring):
+    return re.findall(r"(?<=AUG)(?:[AUGC]{3})+?(?=UAG)", AUGCstring)
+    
