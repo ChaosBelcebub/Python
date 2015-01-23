96ce31330693729df916858a54d63248b2a8259f
ChaosBelcebub <mich3ael@web.de>
2015-01-23 15:37:58 +0100

    commit

--- sheet11/test_dict.py
+++ sheet11/test_dict.py
@@ -5,37 +5,34 @@
    first version and consult the online documentation.
 
 """
-dictionary = {}
+dictionary = dict.fromkeys(range(1000))
 keys_all = list(range(1000))
-keys_half = list(range(500)) + list(range(1000, 1500))
+keys_half = list(range(500, 1500))
 keys_none = list(range(1000, 2000))
-def init():
-    for i in range(1000):
-        dictionary[i] = "test"
 
 
 def func_if_all():
     for k in keys_all:
         if k in dictionary:
-            test = dictionary[k]
+            dictionary[k]
 
 
 def func_if_half():
     for k in keys_half:
         if k in dictionary:
-            test = dictionary[k]
+            dictionary[k]
 
 
 def func_if_none():
     for k in keys_none:
         if k in dictionary:
-            test = dictionary[k]
+            dictionary[k]
 
 
 def func_try_all():
     for k in keys_all:
         try:
-            test = dictionary[k]
+            dictionary[k]
         except:
             pass
 
@@ -43,7 +40,7 @@ def func_try_all():
 def func_try_half():
     for k in keys_half:
         try:
-            test = dictionary[k]
+            dictionary[k]
         except:
             pass
 
@@ -51,13 +48,13 @@ def func_try_half():
 def func_try_none():
     for k in keys_none:
         try:
-            test = dictionary[k]
+            dictionary[k]
         except:
             pass
 
 
 if __name__ == '__main__':
-    init()
+
     import timeit
 
     repeat = 5
@@ -77,13 +74,13 @@ if __name__ == '__main__':
                min(res) / number * unittosec[unit],
                unit))
 
-##    # Second (alternative) version:
-##    for fct in [func_a, func_b, func_c]:
-##        res = timeit.repeat(
-##            fct,
-##            repeat=repeat, number=number)
-##        print("%s: %d loops, best of %d: %.3g %s per loop" %
-##              (fct.__name__,
-##               number, repeat,
-##               min(res) / number * unittosec[unit],
-##               unit))
+#    # Second (alternative) version:
+#    for fct in [func_a, func_b, func_c]:
+#        res = timeit.repeat(
+#            fct,
+#            repeat=repeat, number=number)
+#        print("%s: %d loops, best of %d: %.3g %s per loop" %
+#              (fct.__name__,
+#               number, repeat,
+#               min(res) / number * unittosec[unit],
+#               unit))
